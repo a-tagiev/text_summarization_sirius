8dd2929ed07f041457391060e95ffdb13bbeda92
mikhail shafiev <80474341+mshafiev@users.noreply.github.com>
2024-03-30 14:38:03 +0300

    Delete Server directory

--- Server/helpers/functions.py
+++
@@ -1,8 +0,0 @@
-def make_perfect_answer(summary_answer, select_messages_count):
-    answer = f"""Результат суммаризации {select_messages_count} сообщений:
-
-{summary_answer}
-
-Generate by dataminds 
-    """
-    return answer
