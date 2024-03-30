bc2659b00a01134a9276a4d926df8b31c6a91762
mikhail shafiev <80474341+mshafiev@users.noreply.github.com>
2024-03-30 20:22:30 +0300

    Delete server directory

--- server/vk_bot_output/vk_bot.py
+++
@@ -1,9 +0,0 @@
-import vk_api
-from . token_bot import main_token
-
-vk_session = vk_api.VkApi(token=main_token)
-vk = vk_session.get_api()
-
-
-def sender(peer_id, message):
-    vk.messages.send(peer_id=peer_id, message=message, random_id=0)
