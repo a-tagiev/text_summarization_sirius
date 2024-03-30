266bb25cbc889f95a98398c7dbed013b5ff8d036
mikhail shafiev <80474341+mshafiev@users.noreply.github.com>
2024-03-30 20:22:24 +0300

    Delete VKBot directory

--- VKBot/helpers/bot_functions.py
+++
@@ -1,5 +0,0 @@
-def find_tagged_message(event):
-    # Проверяем, что сообщение содержит тег
-    if 'reply_message' in event.message and 'action' not in event.message:
-        return event.message['reply_message']
-    return None
