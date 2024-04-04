02fa48aeab2cfff281fbd6ce17fae7904bb0dfaa
a-tagiev <66789705+a-tagiev@users.noreply.github.com>
2024-04-04 18:42:46 +0300

    код ревью

--- Helpers/tokenBot.py
+++ Helpers/tokenBot.py
@@ -1,2 +1,2 @@
-group_id = "225216088"
-main_token = "vk1.a.22r2Or2m_5-ZpzDxkuhh-FcS1Cja56dfQXwgchZCMbBQNFQY-IROuV_eLJTCVEplutDVDGl7B8iYKoJs6fTrZWvlOQxbG2VtTuL5ubZfpPZ6BB3_kq-rD3En1JQXMbl2t_etlpE-lnUDzV06Nytcbz_KjBwQh_qxR1uSJF6VcMuLr_B5aRVCdQkwoXf_6kwQwgJ-aMpKyVxFsKqnxHlkpQ"
+group_id = "YOUR_GROUP_ID"
+main_token = "YOUR_TOKEN"
